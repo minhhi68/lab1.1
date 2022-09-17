bf4800cab9a85791d4b5f64fee2d1515cc3dfce6
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-17 23:09:40 +0700

    First

---
+++ src/App.js
@@ -0,0 +1,74 @@
+// // import logo from './logo.svg';
+// // import './App.css';
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// import './App.css';
+// import Navigation from './components/Navigation';
+// import Players from './components/Players';
+// import Footer from './components/Footer';
+// function App(){
+//   return (
+//     <div className='App'>
+//     <Navigation />
+//     <Players />
+//     <Footer />
+//     </div>
+//   );
+// }
+
+
+
+// export default App;
+
+// import './App.css';
+// import Footer from './components/Footer';
+// import Main from './components/Main';
+// import Navigation from './components/Navigation';
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Navigation/>
+//       <Main/>
+//       <Footer/>
+//     </div>
+//   );
+// }
+// export default App;
+
+import './App.css';
+import Navigation from './components/Navigation';
+import Films from './components/Films';
+import Footer from './components/Footer';
+function App(){
+  return (
+    <div className='App'>
+    <Navigation />
+    <Films />
+    <Footer />
+    </div>
+  );
+}
+export default App;
+
---
+++ src/components/Films.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react';
+export default class films extends Component {
+    render(){
+        return(
+            <div className='container'>
+                <div className='column'>
+                    <div className='card'>
+                     <img src='assets/movies/13rw.jpg'/>
+                     <h3>13 Reasons why</h3>
+                     <p className='title'>2018</p>
+                     <p><button>USA</button></p>
+                     </div>                  
+
+                    </div>
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/avengers.jpg'/>
+                            <h3>Avengers: Endgame</h3>
+                            <p className='title'>2019</p>
+                            <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/breakingbad.jpg'/>
+                             <h3>Breaking Bad</h3>
+                             <p className='title'>2008</p>
+                             <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/dune.jpg'/>
+                             <h3>Dune</h3>
+                             <p className='title'>2021</p>
+                             <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/euphoria.jpg'/>
+                            <h3>Euphoria</h3>
+                            <p className='title'>2021</p>
+                            <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/reptour.jpg'/>
+                            <h3>Reputation Tour by Taylor Swift</h3>
+                            <p className='title'>2019</p>
+                            <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/sexeducation.jpg'/>
+                            <h3>Sex Education</h3>
+                            <p className='title'>2018</p>
+                            <p><button>USA</button></p>
+                        </div>
+                    </div>
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/yourname.jpg'/>
+                            <h3>Your name</h3>
+                            <p className='title'>2016</p>
+                            <p><button>Japan</button></p>
+                        </div>
+                    </div>
+
+
+                    <div className='column'>
+                        <div className='card'>
+                            <img src='assets/movies/japansinks.jpg'/>
+                            <h3>Japan sinks</h3>
+                            <p className='title'>2020</p>
+                            <p><button>Japan</button></p>
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+        )
+    }
+
+}
