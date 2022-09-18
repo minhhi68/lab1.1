19375f913dda8c06e88000ea6d47ac0f7b02512f
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-18 08:57:59 +0700

    Components Part 2

---
+++ src/shared/ListOfFilms.js
@@ -0,0 +1,11 @@
+export const Films = [
+    {id: 1, title: '13 Reasons Why', year: '2018', nation: 'USA', img:'assets/movies/13rw.jpg'},
+    {id: 2, title: 'Avengers: Endgame', year: '2019', nation: 'USA', img:'assets/movies/avengers.jpg'},
+    {id: 2, title: 'Breaking Bad', year: '2008', nation: 'USA', img:'assets/movies/breakingbad.jpg'},
+    {id: 2, title: 'Dune', year: '2021', nation: 'USA', img:'assets/movies/dune.jpg'},
+    {id: 2, title: 'Euphoria', year: '2021', nation: 'USA', img:'assets/movies/euphoria.jpg'},
+    {id: 2, title: 'Reputation Tour by Taylor Swift', year: '2019', nation: 'USA', img:'assets/movies/reptour.jpg'},
+    {id: 2, title: 'Sex Education', year: '2018', nation: 'USA', img:'assets/movies/sexeducation.jpg'},
+    {id: 2, title: 'Your name', year: '2016', nation: 'Japan', img:'assets/movies/yourname.jpg'},
+    {id: 2, title: 'Japan sinks', year: '2020', nation: 'Japan', img:'assets/movies/japansinks.jpg'}
+];
