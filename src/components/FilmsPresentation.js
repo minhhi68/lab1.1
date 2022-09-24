affae81fbcd3db03e44d57d5cbc2b2610d914a1b
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-24 13:23:53 +0700

    Hover update

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -1,20 +1,4 @@
-// import React from 'react'
-// export default function FilmsPresentation({film}) {
-//     // console.log(film);
-//     return (
-//      <div className='container'>
-//         {film.map((film)=>(
-//         <div className='column'>
-//         <div className='card'>
-//         <img src={film.img}/>
-//           <h3>{film.title}</h3>
-//           <p className='title'>{film.year}</p>
-//           <p><button>{film.nation}</button></p>
-//         </div>
-//       </div>
-//      ))}</div>   
-//   )
-// }
+
 
 
 import React from 'react'
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,4 @@
-// import React, { Component } from 'react'
-// import { Players } from '../shared/ListOfPlayers'
-// import PlayersPresentation from './PlayersPresentation';
-// export class Main extends Component {
-//     constructor() {
-//         super();
-//         this.state = {
-//            players: Players
-//         };
-//     }
-//     render() {
-//       return <PlayersPresentation players={this.state.players}/>
-//     }
-//   }
-//   export default Main
+
 import React, { Component } from 'react'
 import { Films } from '../shared/ListOfFilms'
 import FilmsPresentation from './FilmsPresentation';
