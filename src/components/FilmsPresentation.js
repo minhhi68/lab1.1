ab9fee9da544ea87afdcf49017f3dc8840e43767
minhhi68 <80388522+minhhi68@users.noreply.github.com>
2022-09-24 09:10:35 +0700

    Lab 2 React Hook Part 1

--- src/components/FilmsPresentation.js
+++ src/components/FilmsPresentation.js
@@ -1,21 +1,61 @@
-import React from 'react'
-export default function FilmsPresentation({film}) {
-     console.log(film);
-    return (
-     <div className='container'>
-        {film.map((film)=>(
-        <div className='column'>
-        <div className='card'>
-        <img src={film.img}/>
-          <h3>{film.title}</h3>
-          <p className='title'>{film.year}</p>
-          <p><button>{film.nation}</button></p>
-        </div>
-      </div>
-     ))}</div>   
-  )
-}
+// import React from 'react'
+// export default function FilmsPresentation({film}) {
+//     // console.log(film);
+//     return (
+//      <div className='container'>
+//         {film.map((film)=>(
+//         <div className='column'>
+//         <div className='card'>
+//         <img src={film.img}/>
+//           <h3>{film.title}</h3>
+//           <p className='title'>{film.year}</p>
+//           <p><button>{film.nation}</button></p>
+//         </div>
+//       </div>
+//      ))}</div>   
+//   )
+// }
 
+
+import React from 'react'
+import { Films } from '../shared/ListOfFilms'
+import { useState } from 'react'
+export default function Players() {
+  const [film, setFilm] = useState([])
+  
+    return (
+    <div className='container'>
+        {Films.map((film)=>(
+           <div className='column' key={film.id}>
+           <div className='card'>
+           <img src={film.img} alt = ''/>
+             <h3>{film.title}</h3>
+             <p className='title'>{film.year}</p>
+             {/* <p><button>{film.nation}</button></p> */}
+             <button onClick={()=>{setFilm(film)}}>
+                        <a href='#popup1' id='openPopUp'>{film.nation}</a>
+
+                    </button>
+
+           </div>
+         </div>
+        ))}
+
+<div id ='popup1' className='overlay'>
+            <div className='popup'>
+                <img src ={film.img}/>
+                <h2>{film.title}</h2>
+                <a className='close' href='#'>&times;</a>
+                <div className='content'>
+                    {film.info}
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+  )
+        }
 
 
 
